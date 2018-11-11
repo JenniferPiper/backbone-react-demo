fac28fe5a4676c5ff0a2c7ac430c42d14a16556c
Jennifer Piper <jennifer@squareandplum.com>
2018-11-10 21:37:59 -0800

    renders JSX directly from ReactDOM.render. Components still do not work. Also installed create-react-class package.

--- src/app.view.js
+++ src/app.view.js
@@ -4,14 +4,24 @@ var _ = require('underscore');
 var Backbone = require('backbone');
 var React = require('react');
 var ReactDOM = require('react-dom');
+var createReactClass = require('create-react-class');
 
 var FormContainer = require('./js/components/container/FormContainer');
 
+var myComponent = createReactClass({
+  render: function() {
+    return '<h1>Text from myComponent</h1>';
+  }
+});
 
-
+class anotherComponent extends React.Component {
+  render() {
+    return <h2>This text comes from anotherComponent.</h2>;
+  }
+}
 
 // Import the template
-//var rawTemplate = require('./app.view.tpl');
+var rawTemplate = require('./app.view.tpl');
 
 
 var viewOptions = {
@@ -19,7 +29,7 @@ var viewOptions = {
 
   initialize: function () {
     // Compile the template
-    //this.template = _.template(rawTemplate);
+    this.template = _.template(rawTemplate);
 
     // Render the view
     this.render();
@@ -27,15 +37,15 @@ var viewOptions = {
 
   render: function () {
     // Render the template
-    //var renderedHtml = this.template( { message: 'App Ready, Now With Templates!' });
-
-    var renderedHtml = '<div id="create-article-form">This text from app.view.js</div>';
+    var renderedHtml = this.template( { message: 'App Ready, Now With Templates!' });
 
     // Update the DOM
-    this.$el.html(renderedHtml);
+    this.$el.html(renderedHtml);  
+
     const wrapper = document.getElementById('create-article-form');
-    wrapper.textContent += 'yo sup';
-  // ReactDOM.render(<FormContainer />, wrapper);
+    wrapper.textContent += ' Eventually.';
+   // ReactDOM.render(<FormContainer />, wrapper);
+   ReactDOM.render(<h1>Does this work</h1>, wrapper);
 
   }
 };
