e05e8473fad8aa8edce1fa4649cb798ff603dee3
Jennifer Piper <jennifer@squareandplum.com>
2018-11-11 21:33:22 -0800

    add some Sass

--- src/index.js
+++ src/index.js
@@ -5,11 +5,8 @@ var AppView = require('./app.view');
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-//var FormContainer = require('./js/components/container/FormContainer');
+require('./styles/main.scss');
 
 Backbone.$(function () {
   new AppView();
 });
-
-//const wrapper = document.getElementById('create-article-form');
-//ReactDOM.render(<FormContainer />, wrapper);
--- src/js/components/presentational/Input.js
+++ src/js/components/presentational/Input.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 const Input = ({ label, text, type, id, value, handleChange }) => (
   <div className='form-group'>
     <label htmlFor={label}>{text}</label>
+    <br/>
     <input
       type={type}
       className='form-control'
